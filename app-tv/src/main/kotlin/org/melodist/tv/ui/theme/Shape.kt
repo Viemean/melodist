87644b6390c595ce508bc3aa458e587f3a4f51d5
Yuzuki <lxf74663@gmail.com>
2026-09-13 20:29:32 +0800

    style(tv): define global DialogCorner in MelodistShapes

--- app-tv/src/main/kotlin/org/melodist/tv/ui/theme/Shape.kt
+++ app-tv/src/main/kotlin/org/melodist/tv/ui/theme/Shape.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.unit.dp
 
 object MelodistShapes {
+    val DialogCorner = RoundedCornerShape(16.dp)
     val CardCorner = RoundedCornerShape(12.dp)
     val ButtonCorner = RoundedCornerShape(12.dp)
     val PillCorner = RoundedCornerShape(12.dp)
