943b731a293b200b48a5cb2c0d18d0d9c7340923
Yuzuki <lxf74663@gmail.com>
2026-09-20 02:05:25 +0800

    chore(release): bump version to 1.3.7

--- app-mobile/build.gradle.kts
+++ app-mobile/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "org.melodist.mobile"
         minSdk = 28
         targetSdk = 36
-        versionCode = 136
-        versionName = "1.3.6"
+        versionCode = 137
+        versionName = "1.3.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
--- app-tv/build.gradle.kts
+++ app-tv/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "org.melodist.tv"
         minSdk = 28
         targetSdk = 36
-        versionCode = 136
-        versionName = "1.3.6"
+        versionCode = 137
+        versionName = "1.3.7"
 
         buildConfigField("String", "GRADLE_VERSION", "\"${gradle.gradleVersion}\"")
         buildConfigField("String", "AGP_VERSION", "\"${libs.versions.agp.get()}\"")
