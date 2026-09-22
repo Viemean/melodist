f4edb11e93ba73b0b8101d8623fc43c226aa9347
Yuzuki <lxf74663@gmail.com>
2026-09-22 22:22:08 +0800

    chore: bump version to 1.4.4

--- app-tv/build.gradle.kts
+++ app-tv/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "org.melodist.tv"
         minSdk = 28
         targetSdk = 37
-        versionCode = 143
-        versionName = "1.4.3"
+        versionCode = 144
+        versionName = "1.4.4"
 
         buildConfigField("String", "GRADLE_VERSION", "\"${gradle.gradleVersion}\"")
         buildConfigField("String", "AGP_VERSION", "\"${libs.versions.agp.get()}\"")
